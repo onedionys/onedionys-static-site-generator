eaf12e1113f0ab657409ca11825378a93a16025c
One Dionys <161098412+onedionys@users.noreply.github.com>
2024-03-17 23:32:17 +0700

    chore: add test package before publishing

---
+++ test/index.test.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+const { generateSite } = require('../src/generator');
+
+describe('Static Site Generator', function() {
+  it('should generate HTML files from markdown files', function() {
+    // Your test case here
+    assert.strictEqual(true, true);
+  });
+});
